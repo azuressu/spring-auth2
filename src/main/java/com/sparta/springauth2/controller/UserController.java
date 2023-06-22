19a185075cf42ae55b5b5108ac8a53bac27b54da
azuressu <martina0130@naver.com>
2023-06-22 17:06:18 +0900

    로그인 구현

--- src/main/java/com/sparta/springauth2/controller/UserController.java
+++ src/main/java/com/sparta/springauth2/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sparta.springauth2.controller;
 
+import com.sparta.springauth2.dto.LoginRequestDto;
 import com.sparta.springauth2.dto.SignupRequestDto;
 import com.sparta.springauth2.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,4 +35,13 @@ public class UserController {
         return "redirect:/api/user/login-page";
     }
 
+    @PostMapping("/user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res) {
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return "redirect:/api/user/login-page?error";
+        }
+        return "redirect:/";
+    }
 }
--- src/main/java/com/sparta/springauth2/service/UserService.java
+++ src/main/java/com/sparta/springauth2/service/UserService.java
@@ -1,10 +1,12 @@
 package com.sparta.springauth2.service;
 
+import com.sparta.springauth2.dto.LoginRequestDto;
 import com.sparta.springauth2.dto.SignupRequestDto;
 import com.sparta.springauth2.entity.User;
 import com.sparta.springauth2.entity.UserRoleEnum;
+import com.sparta.springauth2.jwt.JwtUtil;
 import com.sparta.springauth2.repository.UserRepository;
-import org.springframework.security.core.parameters.P;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,12 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     // ADMIN - TOKEN
@@ -28,8 +32,9 @@ public class UserService {
         String password = passwordEncoder.encode(requestDto.getPassword());
 
         // 회원 중복 확인
+        // isPresent - 현재 이 Optional에 넣어준 값이 존재하는지 안하는지를 확인 (있으면 true, 없으면 false)
         Optional<User> checkUsername = userRepository.findByUsername(username);
-        if (checkUsername.isPresent()) { // 현재 이 Optional에 넣어준 값이 존재하는지 안하는지를 확인 (있으면 true, 없으면 false)
+        if (checkUsername.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
         }
 
@@ -37,14 +42,14 @@ public class UserService {
         String email = requestDto.getEmail();
         Optional<User> checkEmail = userRepository.findByEmail(email);
         if (checkEmail.isPresent()) {
-            throw new IllegalArgumentException("중복된 email 입니다.");
+            throw new IllegalArgumentException("중복된 Email 입니다.");
         }
 
         // 사용자 ROLE 확인
         UserRoleEnum role = UserRoleEnum.USER;
         if (requestDto.isAdmin()) {
             if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
-                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가합니다.");
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
             }
             role = UserRoleEnum.ADMIN;
         }
@@ -52,6 +57,24 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
 
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, res);
     }
 }
