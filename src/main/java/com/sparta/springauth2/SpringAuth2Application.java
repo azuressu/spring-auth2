a47e0416ede9c170a82c94e95c1b277f2f14e485
azuressu <martina0130@naver.com>
2023-06-22 11:30:57 +0900

    init commit

---
+++ src/main/java/com/sparta/springauth2/SpringAuth2Application.java
@@ -0,0 +1,14 @@
+package com.sparta.springauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
+public class SpringAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springauth2/SpringAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
